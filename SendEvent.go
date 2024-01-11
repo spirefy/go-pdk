0cc8bd26aaa026bb1027c56f6ce64beee67c5c48
Kevin Duffey <apifrst@gmail.com>
2024-01-10 22:11:52 -0800

    updated

--- SendEvent.go
+++ SendEvent.go
@@ -1,22 +1,20 @@
 package hostfuncs
 
+import (
+	"github.com/extism/go-pdk"
+)
+
 //go:wasmimport extism:host/user sendevent
 func sendevent(e, d uint64) uint64
 
 func SendEvent(event string, data []byte) {
 	// Call the host function
-	/*
-		evt := pdk.AllocateString(event)
-		dta := pdk.AllocateBytes(data)
 
+	evt := pdk.AllocateString(event)
+	dta := pdk.AllocateBytes(data)
 
-		offs1 := sendevent(evt.Offset(), dta.Offset())
+	offs1 := sendevent(evt.Offset(), dta.Offset())
 
-
-		mem1 := pdk.FindMemory(offs1)
-
-		buffMem1 := make([]byte, mem1.Length())
-		mem1.Load(buffMem1)
-
-	*/
+	mem1 := pdk.FindMemory(offs1)
+	pdk.OutputMemory(mem1)
 }
